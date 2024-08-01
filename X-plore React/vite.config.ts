3dcb4de297a53e4224d0b6432d5a9d840cd7edf5
yadavmangesh07 <mangeshyadav8736@gmail.com>
2024-08-01 11:50:23 +0530

    refactor: Add __dirname declaration in vite.config.ts

--- X-plore React/vite.config.ts
+++ X-plore React/vite.config.ts
@@ -2,6 +2,8 @@ import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
+declare const __dirname: string; // Add this line
+
 export default defineConfig({
   plugins: [react()],
   resolve: {
