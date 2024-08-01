262003b6b903f55311e9b40dcf523446d0f7b5c2
yadavmangesh07 <mangeshyadav8736@gmail.com>
2024-08-01 11:38:41 +0530

    refactor: Add home endpoint to AppApplication.java

--- app/src/main/java/com/SearchEngine/app/AppApplication.java
+++ app/src/main/java/com/SearchEngine/app/AppApplication.java
@@ -2,10 +2,16 @@ package com.SearchEngine.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-
+@RestController
 public class AppApplication {
+	@GetMapping("/")
+	public String home() {
+		return "Hello Welcome to Search Engine";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(AppApplication.class, args);
