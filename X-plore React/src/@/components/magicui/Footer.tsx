e07a618180b0bcae6f46694bb01bd7fae9a7ff7f
yadavmangesh07 <mangeshyadav8736@gmail.com>
2024-08-13 11:16:16 +0530

    refactor: Update Footer component to include designer name

--- X-plore React/src/@/components/magicui/Footer.tsx
+++ X-plore React/src/@/components/magicui/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div className="absolute border-t-[.1px] border-gray-600 bottom-0 h-[3rem] w-full bg-[#101827] text-center p-3 ">
         
-        <h1 className=" bg-gradient-to-r from-neutral-300 to-stone-400 inline-block text-transparent bg-clip-text text-[.7rem] lg:text-[1rem]">Designed By :
+        <h1 className=" bg-gradient-to-r from-neutral-300 to-stone-400 inline-block text-transparent bg-clip-text text-[.7rem] lg:text-[1rem]">Designed By :&nbsp;
           <a href="https://www.linkedin.com/in/mangesh-yadav-65a437237" target="_blank" > 
           Mangesh Yadav</a>
           </h1>
